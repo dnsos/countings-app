f24acc02e25aed125d562ab3d8d2a19f4ffdbdca
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-02-10 23:56:01 +0100

    chore: autocorrect Rubocop issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Brings the sole locale route to the homepage
   get '/:locale' => 'home#index', :as => 'locale_root'
 
