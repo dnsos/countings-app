0aca553cc17bd41808651fa05f41b6bd821d6e18
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-02-10 23:54:44 +0100

    feat(i18n): add exemplary use of i18n (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Brings the sole locale route to the homepage
+  get '/:locale' => 'home#index', :as => 'locale_root'
 
   # Defines the root path route ("/")
   root 'home#index'
