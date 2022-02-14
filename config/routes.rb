cb3e22446911b38111d99ca2fab413156d9a7aa8
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-02-14 23:38:04 +0100

    feat(auth): add User model
    Includes some manual tweaks to make Devise work with Rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  scope '(:locale)', locale: /en|de/ do
+    devise_for  :users,
+                path: 'auth',
+                path_names: { sign_in: 'login',
+                              sign_out: 'logout',
+                              sign_up: 'register' },
+                controllers: {
+                  sessions: 'users/sessions'
+                }
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Brings the sole locale route to the homepage
