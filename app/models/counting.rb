46f1cd8d7eb2f9569964db5c2e39f52e4745def5
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-02-22 18:45:16 +0100

    feat(countings): validate counting input properly

--- app/models/counting.rb
+++ app/models/counting.rb
@@ -1,6 +1,6 @@
 class Counting < ApplicationRecord
-  validates :title, presence: true
-  validates :ends_at, comparison: { greater_than: :starts_at }
+  validates :title, :starts_at, :ends_at, presence: true
+  validates :ends_at, comparison: { greater_than: :starts_at }, unless: -> { starts_at.blank? }
 
   belongs_to :user
 end
