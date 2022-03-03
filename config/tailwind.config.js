f243d602e14abb7ffe3f6eb1cc0b7375b9de3c5b
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-03 19:42:13 +0100

    style: add initial style and layout for countings

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,53 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*'
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*",
   ],
   theme: {
+    colors: {
+      inherit: "inherit",
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      blue: {
+        50: "#e9ebf3",
+        100: "#d3d8e7",
+        200: "#a6b0cf",
+        300: "#7a89b7",
+        400: "#4d619f",
+        500: "#213a87",
+        600: "#1a2e6c",
+        700: "#142351",
+        800: "#0d1736",
+        900: "#070c1b",
+      },
+      yellow: {
+        50: "#fffbe6",
+        100: "#fff7cc",
+        200: "#ffee99",
+        300: "#ffe666",
+        400: "#ffdd33",
+        500: "#ffd500",
+        600: "#ccaa00",
+        700: "#998000",
+        800: "#665500",
+        900: "#332b00",
+      },
+      ...defaultTheme.colors.red,
+      ...defaultTheme.colors.green,
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
