db03268133608687052c0b687678ea2c8570405b
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-03-11 15:28:13 +0100

    chore: migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_303_232_245) do
+ActiveRecord::Schema[7.0].define(version: 20_220_311_132_826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -61,6 +61,13 @@ ActiveRecord::Schema[7.0].define(version: 20_220_303_232_245) do
     t.datetime 'updated_at', null: false
     t.bigint 'user_id', null: false
     t.index ['user_id'], name: 'index_countings_on_user_id'
+  end
+
+  create_table 'genders', force: :cascade do |t|
+    t.string 'label_de'
+    t.string 'label_en'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
   create_table 'users', force: :cascade do |t|
