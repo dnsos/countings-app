4c9b6f9d6284dcecacdc2c5e2ab7a6da59777245
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-09 14:17:58 +0200

    chore: make Brakeman happy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
-  def switch_locale(&)
+  def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
+    I18n.with_locale(locale, &action)
   end
 
   def default_url_options
@@ -15,8 +15,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin!
     authenticate_user!
-    flash[:alert] =
-      I18n.t('errors.messages.not_authorized') unless current_user
+    flash[:alert] = I18n.t('errors.messages.not_authorized') unless current_user
       .admin?
 
     render 'home/index', status: :not_found unless current_user.admin?
