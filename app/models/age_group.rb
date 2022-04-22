5ce95b4796115372b620c5c64f2d7c047213ec31
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-22 16:01:37 +0200

    feat: provide label to AgeGroup model
    Made up of min_age and max_age

--- app/models/age_group.rb
+++ app/models/age_group.rb
@@ -1,6 +1,11 @@
+# AgeGroup model
 class AgeGroup < ApplicationRecord
   validates :min_age, presence: true
   validates :max_age, comparison: { greater_than: :min_age }
 
   has_many :people
+
+  def label
+    min_age && max_age ? "#{min_age} - #{max_age}" : "#{min_age}+"
+  end
 end
