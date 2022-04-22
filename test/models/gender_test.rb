f300a244b3b8ae526abf21a3af7501bd8e502a13
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-22 15:40:48 +0200

    test: add more model tests

--- test/models/gender_test.rb
+++ test/models/gender_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
+# The GenderTest tests validations for the Gender model:
 class GenderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should reject a gender without a label_de' do
+    gender = Gender.new(label_de: '')
+    gender.valid?
+    assert_not gender.errors[:label_de].empty?
+  end
+
+  test 'should reject a gender without a label_en' do
+    gender = Gender.new(label_en: '')
+    gender.valid?
+    assert_not gender.errors[:label_en].empty?
+  end
+
+  test 'should accept a gender with valid attributes' do
+    gender = Gender.new(label_de: 'Ein Label', label_en: 'A label')
+    gender.valid?
+    assert_empty gender.errors[:label_en]
+    assert_empty gender.errors[:label_de]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
+# Tests the User model
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'new user receives user role per default' do
+    user = User.new
+    user.valid?
+    assert_equal user.role, 'user'
+  end
 end
