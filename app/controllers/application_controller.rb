b556a0e37c17c61c0deaab5756a44279b5ee8f0a
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-29 19:28:26 +0200

    fix: use the thread-safe before_action for changing the locale
    This helps Devise render the correct locale reliably and has no other downsides.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 # Application controller
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  before_action :switch_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+  def switch_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options
