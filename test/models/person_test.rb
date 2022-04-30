bce9bdbc0e4058397ae314ee95d5b0d6fdffffbc
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-04-30 17:35:52 +0200

    test: assert pet_count to be positive number

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
+# PersonTest
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should reject a person with an invalid pet count' do
+    person = Person.new(pet_count: -1)
+    person.valid?
+    assert_not person.errors[:pet_count].empty?
+  end
+
+  test 'should accept a person with a valid pet count' do
+    person = Person.new(pet_count: 1)
+    person.valid?
+    assert_empty person.errors[:pet_count]
+  end
 end
