3f799cc08aaa5b36f0a170bc0df6fe139ffcb14d
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-05-02 22:20:26 +0200

    test: accept District with valid attributes

--- test/models/district_test.rb
+++ test/models/district_test.rb
@@ -13,4 +13,16 @@ class DistrictTest < ActiveSupport::TestCase
     district.valid?
     assert_not district.errors[:geometry].empty?
   end
+
+  test 'should accept a district with valid attributes' do
+    district =
+      District.new(
+        name: 'Some name',
+        geometry:
+          'MULTIPOLYGON (((13.386680 52.525310, 13.387264 52.520037, 13.407143 52.520708, 13.405245 52.525406)))',
+      )
+    district.valid?
+    assert_empty district.errors[:name]
+    assert_empty district.errors[:geometry]
+  end
 end
