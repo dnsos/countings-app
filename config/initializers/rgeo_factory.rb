e984b0525b2ba22b62a7a17a707c7dacfadba82f
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-05-06 16:49:23 +0200

    fix: add RGeo SRID config

--- config/initializers/rgeo_factory.rb
+++ config/initializers/rgeo_factory.rb
@@ -1,5 +1,8 @@
 RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
-  # https://github.com/rgeo/rgeo/issues/218#issuecomment-731884000
   config.default =
-    RGeo::Geographic.spherical_factory(uses_lenient_assertions: true)
+    RGeo::Geographic.spherical_factory(
+      srid: 4326,
+      # https://github.com/rgeo/rgeo/issues/218#issuecomment-731884000
+      uses_lenient_assertions: true,
+    )
 end
