fc6a80bb493f2e1e63bfce8d61ea6b04061f6739
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-05-12 22:54:19 +0200

    test: add note concerning geolocations test

--- test/models/geolocation_test.rb
+++ test/models/geolocation_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
 class GeolocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Currently only associations which are covered by Rails' own testing
 end
