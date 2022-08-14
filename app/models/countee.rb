63e18b2973faf8de01133f3c02251194258c83ad
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-08-14 11:21:32 +0200

    refactor: move validations inline for now

--- app/models/countee.rb
+++ app/models/countee.rb
@@ -1,5 +1,3 @@
-require_relative 'validations/countee_validator'
-
 class Countee < ApplicationRecord
   belongs_to :counting
   belongs_to :district
@@ -37,4 +35,90 @@ class Countee < ApplicationRecord
   validates_with AgeGroupValidator
 
   validates_with GenderValidator
+end
+
+# Ensures age group exists (or is empty):
+class AgeGroupValidator < ActiveModel::Validator
+  def validate(countee)
+    unless countee.age_group_id.blank? || AgeGroup.exists?(countee.age_group_id)
+      countee.errors.add :age_group_id,
+                         I18n.t(
+                           'activerecord.errors.models.countee.attributes.age_group_id.invalid',
+                         )
+    end
+  end
+end
+
+# This is a custom validator that ensures that a countee can only be added while the associated counting is ongoing.
+class CreatedAtValidator < ActiveModel::Validator
+  def validate(countee)
+    unless countee.counting.ongoing?
+      countee.errors.add :created_at,
+                         I18n.t(
+                           'activerecord.errors.models.countee.attributes.created_at',
+                         )
+    end
+  end
+end
+
+# This is a custom validator that ensures that the latitude and longitude of a countee find the associated district.
+class DistrictValidator < ActiveModel::Validator
+  def validate(countee)
+    has_latitude_longitude =
+      countee.latitude.present? && countee.longitude.present?
+
+    containing_districts =
+      District.contains_point?(countee.latitude.to_f, countee.longitude.to_f)
+
+    unless has_latitude_longitude
+      # First, clear out the default error for a non-existent, required association:
+      countee.errors.delete(:district)
+
+      # Instead, get more precise:
+      countee.errors.add :district,
+                         :no_lat_lon,
+                         message:
+                           I18n.t(
+                             'activerecord.errors.models.countee.attributes.district.no_lat_lon',
+                           )
+    end
+
+    if has_latitude_longitude && containing_districts.empty?
+      # First, clear out the default error for a non-existent, required association:
+      countee.errors.delete(:district)
+
+      # Instead, get more precise:
+      countee.errors.add :district,
+                         :none_found,
+                         message:
+                           I18n.t(
+                             'activerecord.errors.models.countee.attributes.district.none_found',
+                           )
+    end
+
+    if has_latitude_longitude && containing_districts.length > 1
+      # First, clear out the default error for a non-existent, required association:
+      countee.errors.delete(:district)
+
+      # Instead, get more precise:
+      countee.errors.add :district,
+                         :not_one,
+                         message:
+                           I18n.t(
+                             'activerecord.errors.models.countee.attributes.district.not_one',
+                           )
+    end
+  end
+end
+
+# Ensures gender exists (or is empty):
+class GenderValidator < ActiveModel::Validator
+  def validate(countee)
+    unless countee.gender_id.blank? || Gender.exists?(countee.gender_id)
+      countee.errors.add :gender_id,
+                         I18n.t(
+                           'activerecord.errors.models.countee.attributes.gender_id.invalid',
+                         )
+    end
+  end
 end
