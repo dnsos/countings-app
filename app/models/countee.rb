bc8a5f909311e779c8c8f712ae8f41cc15597f62
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-08-14 11:33:12 +0200

    refactor: move validators before model
    Is this fixing the loading and error and if so why?

--- app/models/countee.rb
+++ app/models/countee.rb
@@ -1,42 +1,3 @@
-class Countee < ApplicationRecord
-  belongs_to :counting
-  belongs_to :district
-
-  belongs_to :gender, optional: true
-  belongs_to :age_group, optional: true
-
-  validates_with CreatedAtValidator
-
-  validates :pet_count,
-            numericality: {
-              greater_than_or_equal_to: 0,
-            },
-            unless: -> { pet_count.blank? }
-
-  attribute :latitude, :decimal
-  attribute :longitude, :decimal
-
-  validates :latitude, :longitude, presence: true, on: :create
-  validates :latitude,
-            numericality: {
-              greater_than_or_equal_to: -90,
-              smaller_than_or_equal_to: 90,
-            },
-            on: :create
-  validates :longitude,
-            numericality: {
-              greater_than_or_equal_to: -180,
-              smaller_than_or_equal_to: 180,
-            },
-            on: :create
-
-  validates_with DistrictValidator, on: :create
-
-  validates_with AgeGroupValidator
-
-  validates_with GenderValidator
-end
-
 # Ensures age group exists (or is empty):
 class AgeGroupValidator < ActiveModel::Validator
   def validate(countee)
@@ -121,4 +82,43 @@ class GenderValidator < ActiveModel::Validator
                          )
     end
   end
+end
+
+class Countee < ApplicationRecord
+  belongs_to :counting
+  belongs_to :district
+
+  belongs_to :gender, optional: true
+  belongs_to :age_group, optional: true
+
+  validates_with CreatedAtValidator
+
+  validates :pet_count,
+            numericality: {
+              greater_than_or_equal_to: 0,
+            },
+            unless: -> { pet_count.blank? }
+
+  attribute :latitude, :decimal
+  attribute :longitude, :decimal
+
+  validates :latitude, :longitude, presence: true, on: :create
+  validates :latitude,
+            numericality: {
+              greater_than_or_equal_to: -90,
+              smaller_than_or_equal_to: 90,
+            },
+            on: :create
+  validates :longitude,
+            numericality: {
+              greater_than_or_equal_to: -180,
+              smaller_than_or_equal_to: 180,
+            },
+            on: :create
+
+  validates_with DistrictValidator, on: :create
+
+  validates_with AgeGroupValidator
+
+  validates_with GenderValidator
 end
