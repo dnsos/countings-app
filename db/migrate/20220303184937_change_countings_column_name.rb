5f7aa2f0475492d760f30806035cc62a2c7ad56e
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-08-15 19:48:53 +0200

    chore: fix wrong column name
    Apparently during a search and replace the first part of this migration function has been renamed by mistake. This fixes that.

--- db/migrate/20220303184937_change_countings_column_name.rb
+++ db/migrate/20220303184937_change_countings_column_name.rb
@@ -1,6 +1,6 @@
 # We also want to have a description_long, so this migration needs to happen first.
 class ChangeCountingsColumnName < ActiveRecord::Migration[7.0]
   def change
-    rename_column :countings, :description_short, :description_short
+    rename_column :countings, :description, :description_short
   end
 end
