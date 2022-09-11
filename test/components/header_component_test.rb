872de0826b8089a5bc382a0ed81ae2565014ddde
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-09-11 14:21:32 +0200

    test(Header): use proper content in component test

--- test/components/header_component_test.rb
+++ test/components/header_component_test.rb
@@ -3,9 +3,28 @@
 require 'test_helper'
 
 class HeaderComponentTest < ViewComponent::TestCase
-  test 'renders its content' do
-    render_inline(HeaderComponent.new) { 'I am in the content area' }
+  # We need to include Rails' tag helpers if using content_tag in the test:
+  include ActionView::Helpers::TagHelper
 
-    assert_text 'I am in the content area'
+  test 'renders its content' do
+    render_inline(HeaderComponent.new) do |component|
+      component.with_language_switch(
+        label: 'Please select a language',
+      ) do |language_component|
+        language_component.with_language_link do
+          content_tag :a, 'EN', href: '/en'
+        end
+
+        language_component.with_language_link do
+          content_tag :a, 'DE', href: '/de'
+        end
+      end
+
+      component.with_link { content_tag :a, 'Home', href: '/' }
+    end
+
+    assert_link('Home', href: '/')
+    assert_link('DE', href: '/de')
+    assert_link('EN', href: '/en')
   end
 end
