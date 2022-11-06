d6430a62edeb09781f8dccd317a99669f2d4e39c
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-11-06 18:23:03 +0100

    test(#84): ensure counting signups are routed correctly

--- app/controllers/counting_signups_controller.rb
+++ app/controllers/counting_signups_controller.rb
@@ -3,14 +3,14 @@ class CountingSignupsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
 
   before_action :set_counting
-  before_action :set_counting_signup
+  before_action :set_counting_signup, only: %i[destroy]
 
   def index; end
 
   def create
     @counting_signup =
       CountingSignup.new(
-        counting_id: counting_signup_params[:counting_id],
+        counting_id: params[:counting_id],
         user_id: current_user.id,
       )
 
@@ -40,15 +40,10 @@ class CountingSignupsController < ApplicationController
   private
 
   def set_counting
-    @counting = Counting.find(counting_signup_params[:counting_id])
+    @counting = Counting.find(params[:counting_id])
   end
 
   def set_counting_signup
-    @counting_signup =
-      @counting.counting_signups.find_by(user_id: current_user.id)
-  end
-
-  def counting_signup_params
-    params.permit(:counting_id)
+    @counting_signup = @counting.counting_signups.find(params[:id])
   end
 end
--- app/models/counting_signup.rb
+++ app/models/counting_signup.rb
@@ -2,5 +2,12 @@ class CountingSignup < ApplicationRecord
   belongs_to :counting
   belongs_to :user
 
-  validates :user, uniqueness: { scope: :counting }
+  validates :user,
+            uniqueness: {
+              scope: :counting,
+              message:
+                I18n.t(
+                  'activerecord.errors.models.counting_signup.attributes.user.uniqueness',
+                ),
+            }
 end
