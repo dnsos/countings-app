1fdd307f742d838094082c59b64c15ccbbf85708
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-12-11 17:10:10 +0100

    test: reintroduce countee creation tests
    They were failing before because of a missing authorization for regular users to view a counting area, thus it would have not been rendered in the countees form. The authorization has been fixed in one of the previos few commits.

--- test/system/countees_test.rb
+++ test/system/countees_test.rb
@@ -4,8 +4,13 @@ class CounteesTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
 
   setup do
+    @user = users(:regular)
     @counting = countings(:ongoing)
     @countee = countees(:with_all_attributes)
+
+    # This assignment has to match a counting_signup from the abovce @user:
+    @first_area_assignment =
+      area_assignments(:regular_user_ongoing_counting_one)
     I18n.locale = :de
   end
 
@@ -15,50 +20,46 @@ class CounteesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: I18n.t('countees.index.title')
   end
 
-  # TODO: fix the tests that were commented out!
-  # Seems realted to a Turbo/Capybara issue
-  # Works fine in development.
+  test 'creates countee' do
+    sign_in @user
+    visit counting_url(@counting, locale: I18n.locale)
 
-  # test 'creates countee' do
-  #   sign_in users(:regular)
-  #   visit counting_url(@counting, locale: I18n.locale)
+    click_on I18n.t('countees.new.title')
 
-  #   click_on I18n.t('countees.new.title')
+    assert_text @first_area_assignment.counting_area.name
 
-  #   assert_text 'Mitte 1'
+    click_on I18n.t(
+               'helpers.submit.create',
+               model: I18n.t('activerecord.models.countee.one').to_s,
+             )
 
-  #   click_on I18n.t(
-  #              'helpers.submit.create',
-  #              model: I18n.t('activerecord.models.countee.one').to_s,
-  #            )
+    assert_text I18n.t('countees.create.notice')
+  end
 
-  #   assert_text I18n.t('countees.create.notice')
-  # end
+  test 'creates countee after first failing to provide valid pet count' do
+    sign_in users(:regular)
+    visit counting_url(@counting, locale: I18n.locale)
 
-  # test 'creates countee after first failing to provide valid pet count' do
-  #   sign_in users(:regular)
-  #   visit counting_url(@counting, locale: I18n.locale)
+    click_on I18n.t('countees.new.title')
 
-  #   click_on I18n.t('countees.new.title')
+    fill_in I18n.t('activerecord.attributes.countee.pet_count'), with: -1
 
-  #   fill_in I18n.t('activerecord.attributes.countee.pet_count'), with: -1
+    click_on I18n.t(
+               'helpers.submit.create',
+               model: I18n.t('activerecord.models.countee.one').to_s,
+             )
 
-  #   click_on I18n.t(
-  #              'helpers.submit.create',
-  #              model: I18n.t('activerecord.models.countee.one').to_s,
-  #            )
+    assert_text I18n.t('common.error')
 
-  #   assert_text I18n.t('common.error')
+    fill_in I18n.t('activerecord.attributes.countee.pet_count'), with: 1
 
-  #   fill_in I18n.t('activerecord.attributes.countee.pet_count'), with: 1
+    click_on I18n.t(
+               'helpers.submit.create',
+               model: I18n.t('activerecord.models.countee.one').to_s,
+             )
 
-  #   click_on I18n.t(
-  #              'helpers.submit.create',
-  #              model: I18n.t('activerecord.models.countee.one').to_s,
-  #            )
-
-  #   assert_text I18n.t('countees.create.notice')
-  # end
+    assert_text I18n.t('countees.create.notice')
+  end
 
   test 'deletes Countee' do
     sign_in users(:admin)
